85f594d37ef90c329bcd414b7c6edf603f0d94ff
Кристина <duleba@eml.ru>
2022-11-27 19:38:45 +0700

    задача 5 билет

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,9 @@
-public class Main {
-}
 
+public class Main {
+    public static void main ( String [] argos) {
+        String book [] = {"Пушкин А.С." , "Золотая рыбка", "Толстой Л.Н.", "Война и мир"} ;
+        for ( int i = 0; i < book.length; i+=2) {
+            System.out.println (book[i] + " " + book[i + 1]);
+        }
+    }
+}
